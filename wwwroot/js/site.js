a5405711500e24fd38e953f5e0341677dd2e6a76
Josh Hill <148567526+Sahujo-Hill@users.noreply.github.com>
2024-10-25 21:49:05 +0100

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// Initialize all tooltips
+window.initializeTooltips = () => {
+    console.log('Initializing tooltips...');
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);  // bootstrap must be defined here
+    });
+};
