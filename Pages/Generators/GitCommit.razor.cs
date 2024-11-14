cd6278bdefd279353f08db4d4cf25bea3288c733
Josh Hill <josh.hill@aplan.co.uk>
2024-11-14 09:33:10 +0000

    feat(pullRequest): separated code out of markup, added some variables with binding

---
+++ Pages/Generators/GitCommit.razor.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.JSInterop;
+
+namespace GitGenerator.Pages.Generators;
+
+public partial class GitCommit
+{
+    public string commitType { get; set; } = string.Empty;
+    public string commitScope { get; set; } = string.Empty;
+    public string commitDescription { get; set; } = string.Empty;
+
+    public bool breakingChange { get; set; } = false;
+    public string breakingChangeDescription { get; set; } = string.Empty;
+
+    public bool extraFooters { get; set; } = false;
+    public string extraFooterReviewedBy { get; set; } = string.Empty;
+    public string extraFooterReferences { get; set; } = string.Empty;
+
+    public string commitMessage { get; set; }
+
+
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            // Initialize tooltips
+            await JSRuntime.InvokeVoidAsync("initializeTooltips");
+        }
+    }
+
+    private void SetCommitTypeToFix()
+    {
+        commitType = "fix";
+        UpdateCommitMessage();
+    }
+
+    private void SetCommitTypeToFeature()
+    {
+        commitType = "feat";
+        UpdateCommitMessage();
+    }
+
+    private void SetCommitScopeToClient()
+    {
+        commitScope = "client";
+        UpdateCommitMessage();
+    }
+
+    private void SetCommitScopeToApi()
+    {
+        commitScope = "api";
+        UpdateCommitMessage();
+    }
+
+    private void SetCommitScopeToShared()
+    {
+        commitScope = "sharedPackage";
+        UpdateCommitMessage();
+    }
+
+    private void SetBreakingChangeToTrue()
+    {
+        breakingChange = true;
+        UpdateCommitMessage();
+    }
+
+    private void SetBreakingChangeToFalse()
+    {
+        breakingChange = false;
+        ClearBreakingChange();
+        UpdateCommitMessage();
+    }
+
+    private void ClearBreakingChange()
+    {
+        breakingChangeDescription = string.Empty;
+    }
+
+    private void SetExtraFootersToTrue()
+    {
+        extraFooters = true;
+        UpdateCommitMessage();
+    }
+
+    private void SetExtraFootersToFalse()
+    {
+        extraFooters = false;
+        ClearExtraFooters();
+        UpdateCommitMessage();
+    }
+
+    private void ClearExtraFooters()
+    {
+        extraFooterReviewedBy = string.Empty;
+        extraFooterReferences = string.Empty;
+    }
+
+    public void commitConstructor() => UpdateCommitMessage();
+
+    public void UpdateCommitMessage()
+    {
+        commitMessage = BuildCommitMessage();
+    }
+
+    private string BuildCommitMessage()
+    {
+        string scopeSection = string.IsNullOrWhiteSpace(commitScope) ? "" : $"({commitScope})";
+
+        string breakingChangeMarker = breakingChange ? "!" : "";
+        string breakingChangeSection = string.IsNullOrWhiteSpace(breakingChangeDescription) ? "" : $"\n\nBREAKING CHANGE: {breakingChangeDescription}";
+
+        string extraFooterReviews = string.IsNullOrWhiteSpace(extraFooterReviewedBy) ? "" : $"\nReviewed-by: {extraFooterReviewedBy}";
+        string extraFooterRefs = string.IsNullOrWhiteSpace(extraFooterReferences) ? "" : $"\nRef: {extraFooterReferences}";
+        string extraFooterSection = extraFooters ? $"\n{extraFooterReviews}{extraFooterRefs}" : "";
+
+        return $"{commitType}{scopeSection}{breakingChangeMarker}: {commitDescription}{breakingChangeSection}{extraFooterSection}";
+    }
+
+    private async Task CopyToClipboard()
+    {
+        await JSRuntime.InvokeVoidAsync("navigator.clipboard.writeText", commitMessage);
+    }
+
+    public void ClearForm()
+    {
+        ClearBreakingChange();
+        ClearExtraFooters();
+
+        commitType = string.Empty;
+        commitScope = string.Empty;
+        commitDescription = string.Empty;
+        breakingChange = false;
+        extraFooters = false;
+
+        UpdateCommitMessage();
+    }
+
+}
---
+++ Pages/Generators/PullRequest.razor.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.JSInterop;
+
+namespace GitGenerator.Pages.Generators;
+
+public partial class PullRequest
+{
+    public string pullRequest { get; set; }
+
+    public bool HasFunctionalityMetAcceptanceCriteria { get; set; }
+    public bool HasUpdatedUnitTests { get; set; }
+    public bool HasUpdatedEndToEndTests { get; set; }
+    public bool HasReviewedSonarQubeIssues { get; set; }
+    public bool HasUpdatedDocumentation { get; set; }
+    public bool HasAddressedSecurityImplications { get; set; }
+    public bool HasAddressedPerformanceImplications { get; set; }
+
+    private List<string> Changes { get; set; } = new List<string>();
+    private string newChange { get; set; } = string.Empty;
+    private void AddChange()
+    {
+        if (!string.IsNullOrWhiteSpace(newChange))
+        {
+            Changes.Add(newChange);
+            newChange = string.Empty;
+            UpdatePullRequest();
+        }
+    }
+    private void RemoveChange(int index)
+    {
+        if (index >= 0 && index < Changes.Count)
+        {
+            Changes.RemoveAt(index);
+            UpdatePullRequest();
+        }
+    }
+
+    private List<string> Test { get; set; } = new List<string>();
+    private string newInstruction { get; set; } = string.Empty;
+    private void AddInstructionToTest()
+    {
+        if (!string.IsNullOrWhiteSpace(newInstruction))
+        {
+            Test.Add(newInstruction);
+            newInstruction = string.Empty;
+            UpdatePullRequest();
+        }
+    }
+    private void RemoveInstructionFromTest(int index)
+    {
+        if (index >= 0 && index < Test.Count)
+        {
+            Test.RemoveAt(index);
+            UpdatePullRequest();
+        }
+    }
+
+    private List<string> RelatedPullRequests { get; set; } = new List<string>();
+    private string newRelatedPullRequest { get; set; } = string.Empty;
+    private void AddRelatedPullRequest()
+    {
+        if (!string.IsNullOrWhiteSpace(newRelatedPullRequest))
+        {
+            RelatedPullRequests.Add(newRelatedPullRequest);
+            newRelatedPullRequest = string.Empty;
+            UpdatePullRequest();
+        }
+    }
+    private void RemoveRelatedPullRequest(int index)
+    {
+        if (index >= 0 && index < RelatedPullRequests.Count)
+        {
+            RelatedPullRequests.RemoveAt(index);
+            UpdatePullRequest();
+        }
+    }
+
+
+    public void PullRequestConstructor() => UpdatePullRequest();
+
+    public void UpdatePullRequest()
+    {
+        pullRequest = BuildPullRequest();
+    }
+
+    private string BuildPullRequest()
+    {
+        string changesSection = string.Join("\n", Changes.Select(change => $"- {change}"));
+
+        string testSection = string.Join("\n", Test.Select((instruction, index) => $"{index + 1}. {instruction}"));
+
+        string relatedPullRequestSection = string.Join("\n", RelatedPullRequests.Select(pullRequest => $"- {pullRequest}"));
+
+        string checklistSection = string.Empty; //To do: add mapping here
+
+        return $"##What changes have been made?\n" +
+               $"{changesSection}\n\n" +
+               $"##How can the changes be tested?\n" +
+               $"{testSection}\n\n" +
+               $"##Related PRs\n" +
+               $"{relatedPullRequestSection}\n\n" +
+               $"##Checklist\n" +
+               $"Before submitting this PR for review, I have:\n" +
+               $"{checklistSection}";
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            // Initialize tooltips
+            await JSRuntime.InvokeVoidAsync("initializeTooltips");
+        }
+    }
+
+    private async Task CopyToClipboard()
+    {
+        await JSRuntime.InvokeVoidAsync("navigator.clipboard.writeText", pullRequest);
+    }
+
+    public void ClearForm()
+    {
+        pullRequest = string.Empty;
+        Changes.Clear();
+        Test.Clear();
+        RelatedPullRequests.Clear();
+    }
+
+}
